26bca0eb3a0637dc09201245fa0074c1eda8ccc5
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2022-02-15 16:47:43 -0500

    PyQt5 update to View

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import sys
 from pymongo import MongoClient
 from model import Model
 from view import View
 from controller import Controller
 
 def main():
-    view = View()
+    view = View(sys)
     model = Model(view) 
     controller = Controller(model)
+    view.display()
 
 if __name__=="__main__":
     main()
--- view.py
+++ view.py
@@ -1,3 +1,18 @@
+from PyQt5.QtWidgets import *
+
 class View:
-  def __init__(self):
-      return
+  def __init__(self, sys):
+      self.sys = sys
+      self.app = QApplication(sys.argv)
+
+      self.window = QWidget()
+      self.window.setWindowTitle('PyQt5 App')
+      self.window.setGeometry(100, 100, 280, 80)
+      self.window.move(60, 15)
+      helloMsg = QLabel('<h1>Maik BIG gay!</h1>', parent=self.window)
+      helloMsg.move(60, 15)
+      #self.window.show()
+
+  def display(self):
+      self.window.show()
+      self.sys.exit(self.app.exec_())
