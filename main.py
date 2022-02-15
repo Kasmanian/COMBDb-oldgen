01cf9e0ae1c656f3372c805e5d4743da5885b644
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2022-02-15 15:58:21 -0500

    Co-authored-by: Cgongidi <Cgongidi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from pymongo import MongoClient
+from model import Model
+from view import View
+from controller import Controller
+
+def main():
+    view = View()
+    model = Model(view) 
+    controller = Controller(model)
+
+if __name__=="__main__":
+    main()
