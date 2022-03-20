20ca9a66637aa241c7d99abcfdd5e4cf7dea5333
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2022-03-20 02:14:31 -0400

    Added login to model

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from model import Model
 from view import View
 from controller import Controller
 from schemas import *
+import webbrowser
 
 def main():
-#    myclient = MongoClient("mongodb://localhost:27017/")
-#    mydb = myclient["OMCDb"]
-#    print(myclient.list_database_names())
-#    mycol = mydb["Clinician"]
-#    mydict = Clinician('John', 'Doe', 'Address 1', 'Address 2', 'NC', 'Chapel Hill', 28390, '9103334444', '9102225555', 'email@url.com')
-#    x = mycol.insert_one(mydict.data)
+    # myclient = MongoClient("mongodb://localhost:27017/")
+    # mydb = myclient["OMCDb"]
+    # print(myclient.list_database_names())
+    # mycol = mydb["Clinician"]
+    # mydict = Clinician('Jane', 'Doe', 'Address 1', 'Address 2', 'NC', 'Chapel Hill', 28390, '9103334444', '9102225555', 'email@url.com')
+    # x = mycol.insert_one(mydict.data)
     view = View()
     model = Model(view)
     controller = Controller(model)
-    view.init()
+    # view.init()
+    #print('Hello, world!')
+    #webbrowser.open('index.html')
+    model.adminLogin('admin1', 'password1')
 
 if __name__=="__main__":
     main()
