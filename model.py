f9c01890db094f7a836249df9de3b22348247292
Kasmanian <46714075+Kasmanian@users.noreply.github.com>
2022-04-19 13:39:15 -0400

    Access query conversion completed.

--- model.py
+++ model.py
@@ -1,6 +1,4 @@
-from pymongo import MongoClient
-from schemas import *
-import bcrypt, math, time
+import bcrypt, math
 from datetime import date
 import pyodbc, json
 
@@ -20,16 +18,22 @@ class Model:
       print(f'Error in connection: {e}')
       return False
 
+  def close(self):
+    self.db.close()
+
   def addTech(self, first, last, middle, username, password):
     # Insert new Admin into the database using a given username and password
     try:
       cursor = self.db.cursor()
       # cursor.execute(f'INSERT INTO Techs(First, Last, Username, Password) VALUES(?, ?, ?, ?, ?, ?))', first, last, middle, username, self.encrypt(password).decode('utf-8'))
+      # query = (
+      #   'INSERT INTO Techs(First, Middle, Last, Username, Password, Active)'
+      #   f'VALUES({first}, {middle}, {last}, {username}, {self.encrypt(password).decode("utf-8")}, Yes)'
+      # )
       query = (
-        'INSERT INTO Techs(First, Middle, Last, Username, Password, Active)'
-        f'VALUES({first}, {middle}, {last}, {username}, {self.encrypt(password).decode("utf-8")}, Yes)'
+        'INSERT INTO Techs(First, Middle, Last, Username, Password, Active) VALUES(?, ?, ?, ?, ?, ?)'
       )
-      cursor.execute(query)
+      cursor.execute(query, first, middle, last, username, self.encrypt(password).decode('utf-8'), 'Yes')
       self.db.commit()
     except (Exception, pyodbc.Error) as e:
       print(f'Error in connection: {e}')
@@ -40,22 +44,24 @@ class Model:
   def addGuest(self, pw, ls):
     # Insert new Guest into the database using:
     # a randomly generated key (pw), a timestamp (ts, hours since epoch to the nearest hundredth), and a lifespan (ls, hours)
-    self.jdrv['Guest'].insert_one({'password': self.encrypt(pw), 'timestmp': math.floor(time.time()/36)/100, 'lifespan': ls})
+    # self.jdrv['Guest'].insert_one({'password': self.encrypt(pw), 'timestmp': math.floor(time.time()/36)/100, 'lifespan': ls})
+    pass
 
   def addPatientOrder(self, table, chartID, clinician, first, last, collected, received, comments):
     try:
-      year = 22
+      yy = int(self.date[-1:-2])
       cursor = self.db.cursor()
       query = (
-        f'SELECT COUNT(*) FROM {table} WHERE SampleID >= {year}0000 AND SampleID < {year+1}0000'
+        f'SELECT COUNT(*) FROM {table} WHERE SampleID >= {yy}0000 AND SampleID < {yy+1}0000'
       )
       cursor.execute(query)
-      sampleID = (year * 10000) + cursor.fetchone()[0]+1
+      lastPatientOrder = cursor.fetchone()
+      sampleID = (yy*10000)+lastPatientOrder[0]+1 if lastPatientOrder is not None else (yy*10000)+1
       query = (
-        f'INSERT INTO {table}(SampleID, ChartID, Clinician, First, Last, Collected, Received, Comments)'
-        f'VALUES({sampleID}, {chartID}, {clinician}, {first}, {last}, {collected}, {received}, {comments})'
+        f'INSERT INTO {table}(SampleID, ChartID, Clinician, First, Last, Collected, Received, Comments) VALUES(?, ?, ?, ?, ?, ?, ?, ?)'
       )
-      cursor.execute(query)
+      #  f'VALUES({sampleID}, {chartID}, {clinician}, {first}, {last}, {collected}, {received}, {comments})'
+      cursor.execute(query, sampleID, chartID, clinician, first, last, collected, received, comments)
       self.db.commit()
     except (Exception, pyodbc.Error) as e:
       print(f'Error in connection: {e}')
@@ -63,26 +69,65 @@ class Model:
     finally:
       cursor.close()
 
-  def addCATResult(self):
-    pass
+  def addCATResult(self, sampleID, chartID, clinician, first, last, reported, volume, time, flow, pH, bc, sm, lb, comments):
+    try:
+      cursor = self.db.cursor()
+      # query = (
+      #   'UPDATE CATs '
+      #   f'SET ChartID={chartID}, Clinician={clinician}, First={first}, Last={last}, Tech={self.tech[0]}, Reported={reported}, ' 
+      #   f'[Volume (ml)]={volume}, [Time (min)]={time}, [Flow Rate (ml/min)]={flow}, pH={pH}, '
+      #   f'[Buffering Capacity (pH)]={bc}, [Strep Mutans (CFU/ml)]={sm}, [Lactobacillus (CFU/ml)]={lb}, Comments={comments} '
+      #   f'WHERE SampleID={sampleID}'
+      # )
+      query = (
+        'UPDATE CATs '
+        f'SET ChartID=?, Clinician=?, First=?, Last=?, Tech=?, Reported=?, [Volume (ml)]=?, [Time (min)]=?, [Flow Rate (ml/min)]=?, pH=?, '
+        f'[Buffering Capacity (pH)]=?, [Strep Mutans (CFU/ml)]=?, [Lactobacillus (CFU/ml)]=?, Comments=? WHERE SampleID=?'
+      )
+      cursor.execute(query, chartID, clinician, first, last, self.tech[0], reported, volume, time, flow, pH, bc, sm, lb, comments, sampleID)
+      self.db.commit()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return False
+    finally:
+      cursor.close()
 
-  def addCultureResult():
-    pass
+  def addCultureResult(self, sampleID, chartID, clinician, first, last, reported, results, comments):
+    try:
+      cursor = self.db.cursor()
+      # query = (
+      #   'UPDATE Cultures '
+      #   f'SET ChartID={chartID}, Clinician={clinician}, First={first}, Last={last}, Tech={self.tech[0]}, Reported={reported}, Results={results}, Comments={comments} '
+      #   f'WHERE SampleID={sampleID}'
+      # )
+      query = (
+        'UPDATE Cultures SET ChartID=?, Clinician=?, First=?, Last=?, Tech=?, Reported=?, Results=?, Comments=? WHERE SampleID=?'
+      )
+      cursor.execute(query, chartID, clinician, first, last, self.tech[0], reported, results, comments, sampleID)
+      self.db.commit()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return False
+    finally:
+      cursor.close()
   
   def addWaterlineOrder(self, clinician, shipped, comments):
     try:
-      year = 22
+      yy = int(self.date[-1:-2])
       cursor = self.db.cursor()
       query = (
-        f'SELECT COUNT(*) FROM Waterlines WHERE SampleID >= {year}0000 AND SampleID < {year+1}0000'
+        f'SELECT COUNT(*) FROM Waterlines WHERE SampleID >= {yy}0000 AND SampleID < {yy+1}0000'
       )
       cursor.execute(query)
-      sampleID = (year * 10000) + cursor.fetchone()[0]+1
+      sampleID = (yy*10000)+cursor.fetchone()[0]+1
+      # query = (
+      #   'INSERT INTO Waterlines(SampleID, Clinician, Shipped, Comments)'
+      #   f'VALUES({sampleID}, {clinician}, {shipped}, {comments})'
+      # )
       query = (
-        'INSERT INTO Waterlines(SampleID, Clinician, Shipped, Comments)'
-        f'VALUES({sampleID}, {clinician}, {shipped}, {comments})'
+        'INSERT INTO Waterlines(SampleID, Clinician, Shipped, Comments) VALUES(?, ?, ?, ?)'
       )
-      cursor.execute(query)
+      cursor.execute(query, sampleID, clinician, shipped, comments)
       self.db.commit()
     except (Exception, pyodbc.Error) as e:
       print(f'Error in connection: {e}')
@@ -93,12 +138,15 @@ class Model:
   def addWaterlineReceiving(self, sampleID, operatoryID, clinician, collected, received, product, procedure, comments):
     try:
       cursor = self.db.cursor()
+      # query = (
+      #   'UPDATE Waterlines'
+      #   f'SET OperatoryID={operatoryID}, Clinician={clinician}, Collected={collected}, Received={received}, Product={product}, Procedure={procedure}, Comments={comments}'
+      #   f'WHERE SampleID={sampleID}'
+      # )
       query = (
-        'UPDATE Waterlines'
-        f'SET OperatoryID={operatoryID}, Clinician={clinician}, Collected={collected}, Received={received}, Product={product}, Procedure={procedure}, Comments={comments}'
-        f'WHERE SampleID={sampleID}'
+        'UPDATE Waterlines SET OperatoryID=?, Clinician=?, Collected=?, Received=?, Product=?, Procedure=?, Comments=? WHERE SampleID=?'
       )
-      cursor.execute(query)
+      cursor.execute(query, operatoryID, clinician, collected, received, product, procedure, comments, sampleID)
       self.db.commit()
     except (Exception, pyodbc.Error) as e:
       print(f'Error in connection: {e}')
@@ -106,22 +154,80 @@ class Model:
     finally:
       cursor.close()
 
-  def addWaterlineResult():
-    pass
+  def addWaterlineResult(self, sampleID, clinician, reported, count, cdcada, comments):
+    try:
+      cursor = self.db.cursor()
+      # query = (
+      #   'UPDATE Waterlines'
+      #   f'SET Clinician={clinician}, Reported={reported}, [Bacterial Count]={count}, [CDC/ADA]={cdcada}, Comments={comments}'
+      #   f'WHERE SampleID={sampleID}'
+      # )
+      query = (
+        'UPDATE Waterlines SET Clinician=?, Reported=?, [Bacterial Count]=?, [CDC/ADA]=?, Comments=? WHERE SampleID=?'
+      )
+      cursor.execute(query, clinician, reported, count, cdcada, comments, sampleID)
+      self.db.commit()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return False
+    finally:
+      cursor.close()
 
-  def findSample(sampleType, sampleID):
-    pass
+  def findSample(self, sampleType, sampleID):
+    query = ' WHERE SampleID=?'
+    if sampleType == 'CAT':
+      query = 'SELECT * FROM CATs' + query
+    elif sampleType == 'Culture':
+      query = 'SELECT * FROM Cultures' + query
+    elif sampleType == 'DUWL':
+      query = 'SELECT * FROM Waterlines' + query
+    else:
+      return None
+    try:
+      cursor = self.db.cursor()
+      cursor.execute(query, sampleID)
+      return cursor.fetchone()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return None
+    finally:
+      cursor.close()
+
+  def findClinician(self, entry):
+    try:
+      cursor = self.db.cursor()
+      query = 'SELECT * FROM Clinicians WHERE Entry=?'
+      cursor.execute(query, entry)
+      return cursor.fetchone()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return None
+    finally:
+      cursor.close()
+
+  def selectClinicians(self):
+    try:
+      cursor = self.db.cursor()
+      query = 'SELECT * FROM Clinicians'
+      cursor.execute(query)
+      return cursor.fetchall()
+    except (Exception, pyodbc.Error) as e:
+      print(f'Error in connection: {e}')
+      return None
+    finally:
+      cursor.close()
 
   def techLogin(self, username, password):
     # Pull from Techs table matching & validating user input
     try:
       cursor = self.db.cursor()
-      query = (
-        f'SELECT * FROM Techs WHERE username = {username}'
-      )
-      cursor.execute(query)
+      query = 'SELECT * FROM Techs WHERE username=?'
+      cursor.execute(query, username)
       for tech in cursor.fetchall():
-        if bcrypt.checkpw(password.encode('utf-8'), tech[4].encode('utf-8')):
+        print(tech)
+        if bcrypt.checkpw(password.encode('utf-8'), tech[5].encode('utf-8')) and tech[6] == 'Yes':
+          self.date = date.today().strftime('%m/%d/%y')
+          self.tech = tech
           return True
         return False
     except (Exception, pyodbc.Error) as e:
@@ -132,9 +238,10 @@ class Model:
 
   def guestLogin(self, pw):
     # Create Guest object modeled in schemas.py from the results of fetching a Guest using password (pw)
-    guest = Guest(self.jdrv['Guest'].find_one({'password': self.encrypt(pw)}))
+    # guest = Guest(self.jdrv['Guest'].find_one({'password': self.encrypt(pw)}))
     # If Guest's data is None, there is no such account; bcrypt will validate the password and check expiry if the account exists
-    return guest.data is not None and bcrypt.checkpw(pw, guest.data['password']) and math.floor(time.time()/36)/100-guest.data['timestamp']>guest.data['lifespan']
+    # return guest.data is not None and bcrypt.checkpw(pw, guest.data['password']) and math.floor(time.time()/36)/100-guest.data['timestamp']>guest.data['lifespan']
+    pass
 
   def encrypt(self, token):
     # Salt and hash token (tk)
--- transfer.py
+++ transfer.py
@@ -1,4 +1,4 @@
-import pyodbc, bcrypt, re
+import pyodbc, bcrypt, re, datetime
 
 def addTech(first, middle, last, username, password):
     CONSTR = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\simmsk\Desktop\COMBDb_Test.accdb'
@@ -18,18 +18,28 @@ def convertPractices():
     cursor2 = db2.cursor()
     practices = cursor1.fetchall()
     for row in practices:
-        title = row[3].lower().replace('.', '').capitalize() if row[3] is not None else None
-        comment_fax = None
-        if row[17] is not None:
-            row[17] = re.sub('[^0-9a-zA-Z]+', '', row[17]).lower()
-            if 'fax' in row[17]:
-                comment_fax = re.sub('[^0-9]+', '', row[17])
-        fax = row[13] if row[13] is not None else comment_fax
-        designation = None
-        if row[3] is None:
-            if True:
-                pass
-        cursor2.execute('INSERT INTO Clinicians(Prefix, First, Last, )')
+        query = (
+            'INSERT INTO Clinicians(Prefix, First, Last, Phone, Fax, Email, [Address 1], [Address 2], City, State, Zip, Enrolled, Inactive, Comments, OldID) '
+            f'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        )
+        # enrolled = datetime.datetime(int(row[15][6:]), int(row[15][0:2]), int(row[15][3:5]), 0, 0) if row[15] != '' else None
+        # inactive = datetime.datetime(int(row[16][6:]), int(row[16][0:2]), int(row[16][3:5]), 0, 0) if row[16] != '' else None
+        enrolled = None
+        if row[15] != None and row[15] != '':
+            d1 = row[15].replace('-', '/').replace('*', '').split('/')
+            if len(d1) == 3:
+                if d1[2] != '':
+                    d1[2] = 2000+int(d1[2]) if int(d1[2])<1900 else int(d1[2])
+                    enrolled = datetime.datetime(d1[2], int(d1[0]), int(d1[1]))
+        inactive = None
+        if row[16] != None and row[16] != '':
+            d1 = row[16].replace('-', '/').replace('*', '').split('/')
+            if len(d1) == 3:
+                if d1[2] != '':
+                    d1[2] = 2000+int(d1[2]) if int(d1[2])<1900 else int(d1[2])
+                    enrolled = datetime.datetime(d1[2], int(d1[0]), int(d1[1]))
+        cursor2.execute(query, row[3], row[2], row[1], row[12], row[13], row[14], row[7], row[8], row[9], row[10], row[11], enrolled, inactive, row[17], row[0])
+        db2.commit()
     cursor1.close()
     cursor2.close()
     db1.close()
@@ -46,8 +56,19 @@ def convertData():
     for row in cursor1.fetchall():
         if row[0] < 20000:
             continue
-        if row[4].lower() == 'duwl':
+        elif row[4] == None:
+            continue
+        elif row[4] == '':
+            continue
+        elif row[4].lower() == 'duwl':
             # add to Waterlines table
+            clinician = -1
+            if row[1] != None and row[1] != 0 and row[1] != '':
+                cursor2.execute(f'SELECT * FROM Clinicians WHERE OldID={row[1]}')
+                clinician = cursor2.fetchone()[0]
+            query = (
+                'INSERT INTO Waterlines(SampleID, OperatoryID, Clinician, Tech, Shipped, Collected, Received, )'
+            )
             cursor2.execute('INSERT INTO Waterlines()')
         elif row[4].lower() == 'cat':
             # add to CATs table
@@ -71,6 +92,20 @@ def countRows(year):
         cursor.close()
         db.close()
 
+def selectRow(ID):
+    try:
+        CONSTR = r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\simmsk\Desktop\COPY.CDSNew.mdb'
+        db = pyodbc.connect(CONSTR)
+        cursor = db.cursor()
+        cursor.execute(f'SELECT * FROM [Data Table] WHERE SampleNum={ID}')
+        row = cursor.fetchone()
+        print(row)
+    except (Exception, pyodbc.Error) as e:
+        print(f'Error in connection: {e}')
+    finally:
+        cursor.close()
+        db.close()
+
 def encrypt(token):
     bsalt = bcrypt.gensalt()
     return bcrypt.hashpw(token.encode('utf-8'), bsalt)
@@ -78,4 +113,4 @@ def encrypt(token):
 if __name__=="__main__":
     # addTech('Eric', 'V.', 'Simmons', 'eric_simmons', 'Password')
     # addTech('Shannon', 'M.', 'Reisdorf', 'reisdorf', 'Password')
-    countRows(20)
+    convertData()
